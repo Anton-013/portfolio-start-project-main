7bdf457ea6eb31079310f9f02f1ed848a4b6b6a4
Anton-013 <antonchuvashovoren@mail.ru>
2025-01-31 20:10:18 +0300

    add Menu

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Logo } from '../../components/logo/logo';
+import { Menu } from '../../components/menu/Menu';
+import { Logo } from '../../components/logo/Logo';
 
 export const Header = () => {
     return (
         <StyledHeader>
             <Logo/>
-
-            <nav>
-                <ul>
-                    <li>
-                        <a href="">Home</a>
-                    </li>
-                    <li>
-                        <a href="">Skills</a>
-                    </li>
-                    <li>
-                        <a href="">Works</a>
-                    </li>
-                    <li>
-                        <a href="">Testimony</a>
-                    </li>
-                    <li>
-                        <a href="">Contact</a>
-                    </li>
-                </ul>
-            </nav>
+            <Menu/>            
         </StyledHeader>
     );
 };
