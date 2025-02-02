a94493f25367da6d57b1a11ba511cfdd4d29a49b
Anton-013 <antonchuvashovoren@mail.ru>
2025-02-02 17:59:20 +0300

    add FlexWrapper

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import photo from "../../../assets/images/photo.jpg";
+import { FlexWrapper } from "../../../components/FlexWrapper";
 
 export const Main = () => {
     return (
         <div>
-            <div>
-                <span>Hi There</span>
-                <h2>I am Svetlana Dyablo</h2>
-                <h1>A Web Developer. </h1>
-            </div>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                    <span>Hi There</span>
+                    <h2>I am Svetlana Dyablo</h2>
+                    <h1>A Web Developer. </h1>
+                </div>
 
-            <Photo src={photo} alt=""/>
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
         </div>
     );
 };
