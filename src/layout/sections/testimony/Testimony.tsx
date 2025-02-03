8f7ef21a0e2d8c0723d36e3d0b02b7f09cb56f69
Anton-013 <antonchuvashovoren@mail.ru>
2025-02-03 23:09:59 +0300

    add Testimony

---
+++ src/layout/sections/testimony/Testimony.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import { SectionTitle } from "../../../components/SectionTitle";
+import { Icon } from "../../../components/icon/Icon";
+import { Slider } from "../../../components/slider/Slider";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export {};
+
+export const Testimony = () => {
+    return (
+        <StyledTetimony>
+            <SectionTitle>Testimony</SectionTitle>
+            <FlexWrapper direction={"column"} align={"center"}>
+                <Icon iconId={"quote"}/>
+                <Slider/>
+            </FlexWrapper>
+           
+        </StyledTetimony>
+    );
+};
+
+const StyledTetimony = styled.section`
+    background-color: blue;
+`
