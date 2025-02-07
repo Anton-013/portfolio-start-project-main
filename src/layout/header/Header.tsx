7f198a04f1bce39e9cc0cb74cbf93aac56a64ca1
Anton-013 <antonchuvashovoren@mail.ru>
2025-02-07 20:21:58 +0300

    font, Container

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 import { Menu } from '../../components/menu/Menu';
 import { Logo } from '../../components/logo/Logo';
+import { Container } from '../../components/Container';
+import { FlexWrapper } from '../../components/FlexWrapper';
 
 const items = ["Home", "Skills", "Works", "Tetimony", "Contact"]
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>            
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <Logo/>
+                    <Menu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
+                       
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header `
     background-color: #d4ffd3;
-    display: flex;
-    justify-content: space-between;
 `
