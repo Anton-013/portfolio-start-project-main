7d6d9b3de66a40d3dec2c20054d14cc24de1c947
Anton-013 <antonchuvashovoren@mail.ru>
2025-02-10 19:59:21 +0300

    main part 2

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import photo from "../../../assets/images/photo.jpg";
+import { Container } from "../../../components/Container";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={"center"} justify={"space-around"}>
-                <div>
-                    <span>Hi There</span>
-                    <Name>I am Svetlana Dyablo</Name>
-                    <MainTitle>A Web Developer. </MainTitle>
-                </div>
-
-                <Photo src={photo} alt=""/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper align={"center"} justify={"space-between"}>
+                    <div>
+                        <SmallText>Hi There</SmallText>
+                        <Name>I am Svetlana Dyablo</Name>
+                        <MainTitle>A Web Developer. </MainTitle>
+                    </div>
+        
+                    <Photo src={photo} alt=""/>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
@@ -22,6 +25,7 @@ export const Main = () => {
 const StyledMain = styled.section`
     min-height: 100vh;
     background-color: #fff5e7;
+    display: flex;
 `
 
 const Photo = styled.img`
@@ -31,9 +35,18 @@ const Photo = styled.img`
 `
 
 const MainTitle = styled.h1`
-
+    font-weight: 400;
+    font-size: 27px;
 `
 
 const Name = styled.h2`
+    font-family: "Jisefin Sans", sans-serif;
+    font-weight: 700;
+    font-size: 50px;
+    letter-spacing: 0.05em;
+`
 
+const SmallText = styled.h2`
+    font-weight: 400;
+    font-size: 14px;
 `
