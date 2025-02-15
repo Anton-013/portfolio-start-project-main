b0de947f300c81280e4e85c8cc2db375f825d293
Anton-013 <antonchuvashovoren@mail.ru>
2025-02-16 00:32:55 +0300

    add MobileMenu

--- src/layout/header/headerMenu/HeaderMenu.tsx
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -31,7 +31,9 @@ const StyledHeaderMenu = styled.nav`
         justify-content: center;
     }
 
-    
+    @media ${theme.media.tablet} {
+        display: none;
+    }
 `
 
 const Link = styled.a`
