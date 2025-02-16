6d5c5fd0e66b4a3b091e7f27bfd2444604c5a041
Anton-013 <antonchuvashovoren@mail.ru>
2025-02-16 12:20:38 +0300

    Main setting media img

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -9,7 +9,7 @@ export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
+                <FlexWrapper align={"center"} justify={"space-between"} wrap={"wrap"}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Svetlana Dyablo</span></Name>
@@ -43,6 +43,11 @@ const PhotoWrapper = styled.div`
         top: -24px;
         left: 24px;
         z-index: -1;
+
+        @media ${theme.media.mobile} {
+            width: 314px;
+            height: 414px;
+        }
     }
 `
 
@@ -50,6 +55,11 @@ const Photo = styled.img`
     width: 350px;
     height: 430px;
     object-fit: cover;
+
+    @media ${theme.media.mobile} {
+        width: 310px;
+        height: 380px;
+    }
 `
 
 const MainTitle = styled.h1`
